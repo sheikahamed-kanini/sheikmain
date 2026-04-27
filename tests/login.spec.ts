ccec81d03020ce9289a94b3aada761c832a52908
sheikahamedali <sheikahamed.ali@kanini.com>
2026-04-27 13:14:47 +0530

    changes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,21 @@
 import{test,expect} from '@playwright/test'
 import{env} from  '../env/env';
-test.only('login succesful' , async ({page}) =>{
+test.describe('sauce demo', () => {
+test   ('login succesfull' ,async({page}) => {
 await page.goto(env.baseurl);
 await page.locator('[data-test="username"]').fill('standard_user');
 await page.locator('[data-test="password"]').fill('secret_sauce');
 await page.getByRole('button',{name:'Login'}).click();
 await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+})
+test ( 'login invalid credentials',async ({page}) =>{
+    await page.goto(env.baseurl);
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.getByRole('button',{name:'Login'}).click();
+     const errorMessage = page.locator('[data-test="error"]');
+  await expect(errorMessage).toHaveText(
+    'Epic sadface: Sorry, this user has been locked out.');
+  })
+  
 })
