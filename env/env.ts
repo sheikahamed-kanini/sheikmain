0587a383a48e0bbd3d112603dc5ecbcc97b58115
sheikahamedali <sheikahamed.ali@kanini.com>
2026-04-27 12:32:27 +0530

    test

---
+++ env/env.ts
@@ -0,0 +1,3 @@
+export const env={
+    baseurl: 'https://www.saucedemo.com/'
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,9 @@
 import{test,expect} from '@playwright/test'
+import{env} from  '../env/env';
 test.only('login succesful' , async ({page}) =>{
-await page.goto("https://www.saucedemo.com/");
+await page.goto(env.baseurl);
 await page.locator('[data-test="username"]').fill('standard_user');
 await page.locator('[data-test="password"]').fill('secret_sauce');
 await page.getByRole('button',{name:'Login'}).click();
-await expect (page.locator('[data-test="title"]')).toBeVisible('Prodcuts');
+await expect(page.locator('[data-test="title"]')).toHaveText('Products');
 })
